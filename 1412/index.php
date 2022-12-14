5a3e0ab95377d3f638c203811a4e4f36e2a893c5
Técnico em Informática 2022.2 <36127512022.2@senacrjedu.local>
2022-12-14 16:22:04 -0300

    inicio de php

---
+++ 1412/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+
+$num1 = 100;
+$num2 = 200;
+
+$soma = $num1 + $num2;
+
+if ($soma >= 200){ 
+    echo "Maior que 200";
+   }else{ 
+    echo "Menor que 200";
+   }
+
+   echo "<br/> O resultado da soma é {$soma}";
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula de quarta</title>
+</head>
+<body>
+
+</body>
+</html>
